62c0cda69040def4a3fa22d99055be21416533f8
OvOPlant <149712688+OvOPlant@users.noreply.github.com>
2024-01-31 11:24:05 -0500

    rewrite moveflag mod

--- modloader/mods/v1/moveflag.js
+++ modloader/mods/v1/moveflag.js
@@ -3,6 +3,8 @@
     c2_callFunction("execCode", ["globalThis.sdk_runtime = this.runtime"]);
     let runtime = globalThis.sdk_runtime;
     globalThis.sdk_runtime = old;
+    var x = 1512;
+    var y = 288;
     let notify = (title, text, image = "./speedrunner.png") => {
         cr.plugins_.sirg_notifications.prototype.acts.AddSimpleNotification.call(
             runtime.types_by_index.find(
@@ -14,112 +16,134 @@
         );
     };
 
-    x = 1512
-    y = 288
-
     let moveflag = {
         init() {
             document.addEventListener("keydown", (event) => {
+                if (event.code === "Backquote"){
+                    if (event.ctrlKey){
+                        loadCoordsChange();
+                    }
+                }
                 if (event.code === "KeyF") {
                     if (event.shiftKey) {
-                        this.startCycle();
+                        runtime.tickMe(flagTick)
                     }
                 }
                 if (event.code === "KeyG") {
                     if (event.shiftKey) {
-                        this.stopCycle();
+                        runtime.untickMe(flagTick)
                     }
                 }
                 if (event.code === "KeyO") {
                     if (event.shiftKey) {
-                        this.loadFlagCoords();
+                        loadFlagCoords();
                     }
                 }
                 if (event.code === "KeyZ") {
                     if (event.shiftKey) {
-                        this.loadZoomOut();
+                        loadZoomOut();
                     }
                 }
                 if (event.code === "KeyX") {
                     if (event.shiftKey) {
-                        this.loadZoomIn();
+                        loadZoomIn();
                     }
                 }
                 if (event.code === "KeyD") {
                     if (event.shiftKey) {
-                        this.loadDeleteFlag();
+                        loadDeleteFlag();
                     }
                 }
-                if (event.code === "KeyU") {
+                if (event.code === "BracketLeft") {
                     if (event.shiftKey) {
-                        this.loadDeleteSpikes();
-                    }
-                }
-                if (event.code === "KeyI") {
-                    if (event.shiftKey) {
-                        this.loadUndeleteSpikes();
-                    }
-                }
-                if (event.code === "KeyY") {
-                    if (event.shiftKey) {
-                        this.loadSpawnFlag();
+                        loadSpawnFlag();
                     }
                 }
             });
-
-            this.interval = null;
             globalThis.ovoMoveFlag = this;
-            notify("Mod loaded", "Use Shift + F to infinitely move the flag to the chosen coordinates. Use Shift + G to stop.");
-            notify("Mod loaded", "Use Shift + O to view the current coords to see any mistakes. Use the Modloader to change the x and y coords to whatever you want.");
+            notify("Mod loaded", "Use Shift + F to infinitely move the flag to the chosen coordinates. Use Shift + G to stop.", "https://ovoplant.github.io/ovo/versions/reverse/electric.png");
+            notify("Mod loaded", "Use Shift + O to view the current coords to see any mistakes. Press Ctrl + ` to change the flag's coordinates.", "https://ovoplant.github.io/ovo/versions/reverse/electric.png");
         },
+    }
 
-        loadMoveFlag() {
-            ovoModAPI.game.moveInstance(ovoModAPI.game.getFlag(), x, y)
-        },
+    let getFlag = () => {
+        return runtime.types_by_index.find(
+            (x) =>
+                x.name === "EndFlag" ||
+                (x.plugin instanceof cr.plugins_.Sprite &&
+                    x.all_frames &&
+                    x.all_frames[0].texture_file.includes("endflag"))
+        ).instances[0];
+                }
 
-       loadFlagCoords() {
-            ovoModAPI.game.notify("Flag X coordinates", x, "./images/endflag-sheet0.png")
-            ovoModAPI.game.notify("Flag Y coordinates", y, "./images/endflag-sheet0.png")
-        },
-     
-       loadZoomOut() {
-            ovoModAPI.game.getLayer().scale = 0.5
-        },
+    let getLayer = () => {
+    return runtime.running_layout.layers.find(x => x.name === "Layer 0");
+    }
 
-loadZoomIn() {
-            ovoModAPI.game.getLayer().scale = 1
-        },
+    let createFlag = (x, y, layer) => {
+        const spriteType = runtime.types_by_index.find(x => x.plugin instanceof cr.plugins_.Sprite && x.all_frames && x.all_frames[0].texture_file.includes("endflag"));
+    
 
-loadDeleteFlag() {
-            ovoModAPI.game.destroyInstance(ovoModAPI.game.getFlag())
-        },
+        const sprite = runtime.createInstance(spriteType, layer, x, y);
+        sprite.set_bbox_changed();
+        return sprite;
+    }
 
-loadDeleteSpikes() {
-            ovoModAPI.game.runtime.behaviors[5].my_types[0].all_frames[0].hotspotX = 100000
-        },
+    let destroyFlag = (instance) => {
+    return runtime.DestroyInstance(instance);
+    }
 
-loadUndeleteSpikes() {
-            ovoModAPI.game.runtime.behaviors[5].my_types[0].all_frames[0].hotspotX = 0.5
-        },
-loadSpawnFlag() {
-            ovoModAPI.game.createSprite(x, y, null, null, 0, "endflag", "Layer 0")
-        },
+    let moveFlag = (instance, x, y) => {
+        instance.x = x;
+        instance.y = y;
+        instance.set_bbox_changed();
+        return instance;
+    }
+    let inLevel = () => {
+        return runtime.running_layout.sheetname == "Levels";
+    }
 
-
-
-       startCycle(time) {
-            if (!this.interval) {
-                this.interval = setInterval(this.loadMoveFlag, time);
-            }
-        },
-
-        stopCycle() {
-            if (this.interval) {
-                clearInterval(this.interval);
-                this.interval = null;
+    let flagTick = {
+        tick(){
+            if (inLevel()){
+                if (getFlag()){
+            moveFlag(getFlag(), parseFloat(x), parseFloat(y))
+                }
             }
         }
     }
+function loadCoordsChange(){
+    var flagX = prompt("Change the flag's x coordinate to a number.")
+    if (flagX === null || flagX === "" || isNaN(flagX)){
+        alert("Must be a number, X not changed.")
+        return flagX = x
+    }else{
+        x = flagX
+    }
+    var flagY = prompt("Change the flag's y coordinate to a number.")
+    if (flagY === null || flagY === "" || isNaN(flagY)){
+        alert("Must be a number, X not changed.")
+        return flagY = y
+    }else{
+        y = flagY
+    }
+}
+function loadFlagCoords(){
+    notify("Flag X coordinates", x, "./images/endflag-sheet0.png")
+    notify("Flag Y coordinates", y, "./images/endflag-sheet0.png")
+}
+function loadZoomOut(){
+    getLayer().scale = 0.5
+}
+function loadZoomIn(){
+    getLayer().scale = 1
+}
+function loadDeleteFlag(){
+    destroyFlag(getFlag())
+}
+function loadSpawnFlag(){
+    createFlag(parseFloat(x), parseFloat(y), getLayer())
+}
 
     moveflag.init();
 })()
