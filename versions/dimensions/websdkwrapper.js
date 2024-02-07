334d72c81581dd8f010da7bc011481ab6a954618
OvOPlant <149712688+OvOPlant@users.noreply.github.com>
2024-02-07 10:14:53 -0500

    Update websdkwrapper.js

--- versions/dimensions/websdkwrapper.js
+++ versions/dimensions/websdkwrapper.js
@@ -68,7 +68,7 @@ globalThis.WebSdkWrapper = (function () {
       get sdk() {
         return globalThis.PokiSDK;
       },
-      scriptSrc: "poki-sdk.js",
+      scriptSrc: "//game-cdn.poki.com/scripts/v2/poki-sdk.js",
       hasAds: true,
       hasBanner: false,
       enableOnlyInProduction: false,
