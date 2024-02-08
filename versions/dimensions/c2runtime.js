75e87b23df8e64304b978a52a610b3aef9b629a8
OvOPlant <149712688+OvOPlant@users.noreply.github.com>
2024-02-08 08:55:51 -0500

    a

--- versions/dimensions/c2runtime.js
+++ versions/dimensions/c2runtime.js
@@ -37378,7 +37378,7 @@ cr.plugins_.rojo3d = function (runtime) {
   };
   instanceProto.destroyObj = function (tag) {
     if (tag == "" || tag == "camera" || tag == "light" || !(tag in this.objs)) {
-      console.log("error: can't destroy \"" + tag + '".');
+      //console.log("error: can't destroy \"" + tag + '".');
       return;
     }
     var obj = this.objs[tag],
